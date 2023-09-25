8f60675f5e4a5be6c52aa8f8b6722a6de87b56cd
Gopinath Asokan <123052643+gopiashokan@users.noreply.github.com>
2023-09-25 08:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from langchain.chains.question_answering import load_qa_chain
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 import warnings
 warnings.filterwarnings('ignore')
 
